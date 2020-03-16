4ed08b61f5b3b7bd17f1388c082e5a7067d841e7
Singleton <singlet6@egb076000037.lancs.local>
2020-03-16 16:28:28 +0000

    Task 3 finished

--- Board.java
+++ Board.java
@@ -9,11 +9,30 @@ import javax.swing.JFrame;
 
 public class Board extends JFrame
 {
+    ArrayList<Square> Squares = new ArrayList<Square>(); 
     static GraphicsConfiguration Board;
+    String FrogType;
+    Square Square4;
+
     public Board()
     {
         
     }
+    public void FrogPostions(int a,int b, int c,int d, int e,int f)
+        {
+            int CurrentFrogPostion;
+            int[] FrogsPositions = {a,b,c,d,e,f};
+            CurrentFrogPostion = FrogsPositions[5];
+             Square4 = Squares.get(CurrentFrogPostion);
+            Square4.ChangeType("RedFrog");
+            for(int i = 0; i < 5; i++)
+            {
+                CurrentFrogPostion = FrogsPositions[i];
+                Square4 = Squares.get(CurrentFrogPostion);
+                Square4.ChangeType("GreenFrog");
+            }
+            
+        }
     public void Show()
     {
         
@@ -34,8 +53,8 @@ public class Board extends JFrame
         JFrame frame = new JFrame(Board);	
         frame.setSize(700, 600);
 		frame.setLayout(null); 
-        ArrayList<Square> Squares = new ArrayList<Square>(); 
-        Square Square4;
+        
+        
         for(j = 0; j < 5; j++)
         {
            
@@ -74,16 +93,26 @@ public class Board extends JFrame
             Square4.ChangeType("LilyPad");
             i = i + 2;
         }
-        Square4 = Squares.get(6);
-        Square4.ChangeType("GreenFrog");
-        CurentImage = Square4.GetType();
-        buttons[6].setIcon(GreenFrog);
+    
+       
+        FrogPostions(6,8,12,20,24,22);
+       
+        for(i = 0; i < 25; i++)
+        {
+            Square4 = Squares.get(i);
+            FrogType = Square4.GetType();
+            if (FrogType == "RedFrog")
+            {
+                buttons[i].setIcon(RedFrog);
+            }
+            else if(FrogType == "GreenFrog")
+            {
+                buttons[i].setIcon(GreenFrog);
+            }
+            
+            
+        }
+
     }
-    public void PlaceFrogs(int a, int b, int c, int d, int e, int f)
-    {
-        Square4 = Squares.get(6);
-        Square4.ChangeType("GreenFrog");
-        CurentImage = Square4.GetType();
-        buttons[6].setIcon(GreenFrog);
-    }
+   
 }
