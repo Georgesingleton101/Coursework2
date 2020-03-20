2c195d53cece683e1e46f07f8da8536f4c7c6a6b
unknown <GeorgeSingleton100@gmail.com>
2020-03-20 12:55:44 +0000

    Code Restructuring

--- CW2Main.java
+++ CW2Main.java
@@ -2,9 +2,9 @@ public class CW2Main
 {
      public static void main(String[] arguments)
     {
-        
-        Board GameBoard = new Board();
-        GameBoard.Show();
-       
+        Board GameBoard = new Board(1);
+        GameBoard.SetLilyPads();
+        GameBoard.FrogPostions(4,2,6,8,16,18,25,25);
+        GameBoard.SetOnClick();
     }
 }
